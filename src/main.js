c70840fec887acf657cb6c9066a0e423740d8112
code913 <54856929+code913@users.noreply.github.com>
2022-10-23 20:21:24 +0530

    feat: :sparkles: Fully implement gameplay mechanics (#4)

--- src/main.js
+++ src/main.js
@@ -8,19 +8,47 @@ const CELL_TYPES = {
     NUM: "num",
     BOMB: "bomb"
 };
-const LEVELS = {
+const MODES = {
     easy: { name: "easy", bombs: 16 },
     medium: { name: "medium", bombs: 24 },
     hard: { name: "hard", bombs: 32 }
 };
+const EVENTS = {
+    PRIMARYCLICK: "primaryclick",
+    SECONDARYCLICK: "secondaryclick",
+    F: "f",
+    ARROWLEFT: "arrowleft",
+    ARROWUP: "arrowup",
+    ARROWRIGHT: "arrowright",
+    ARROWDOWN: "arrowdown"
+};
 const BOARD_SIZE = [15, 12];
 const MAX_RECUR_DEPTH = 2;
-
+const COORDS = {
+    [EVENTS.ARROWLEFT]: [-1, 0],
+    [EVENTS.ARROWUP]: [0 ,-1],
+    [EVENTS.ARROWRIGHT]: [1 , 0],
+    [EVENTS.ARROWDOWN]: [0 , 1]
+};
+const GAME_PLAY_STATES = {
+    PLAYING: "playing",
+    WINNING: "W",
+    LOSING: "L",
+    ENDED: "ended"
+};
 // #endregion
 
 // #region State variables
-let level = LEVELS.hard;
-let board = generateBoard(...BOARD_SIZE, level.bombs);
+let mode;
+let board;
+let gameState;
+
+resetStates();
+
+setInterval(() => {
+    if (gameState.playState === GAME_PLAY_STATES.PLAYING) gameState.timeElapsed += 69
+}, 69);
+
 // #endregion
 
 // #region Helper functions
@@ -33,13 +61,14 @@ function rand(minmax, exclude) {
         exclude
     ].flat(1);
 
-    let numArr = Array(max - min - exclude.length).fill(0).map((_, i) => i + 1 + min);
+    let numArr = Array(max - min - exclude.length).fill(0).map((_, i) => i + 1 + min).filter(n => !exclude.includes(n));
 
     return numArr[Math.floor(Math.random() * numArr.length)];
 }
 
-function generateBoard(width, height, bombs) {
-    let board = Array(width).fill(Array(height).fill(0)).map((arr, x) => arr.map((_, y) => ({ x, y, type: CELL_TYPES.NUM, hidden: true })));
+function generateBoard() {
+    const [width, height, bombs] = [...BOARD_SIZE, mode.bombs];
+    let board = Array(width).fill(Array(height).fill(0)).map((arr, x) => arr.map((_, y) => ({ x, y, type: CELL_TYPES.NUM, hidden: true, flagged: false })));
 
     Array(bombs).fill(0).reduce(acc => {
         let n = rand(width * height - 1, acc),
@@ -94,64 +123,227 @@ function calculateEmptyCells(cell, curDepth = 1) {
     return accumulator;
 }
 
+function eventHandler(type, event, childInfo) {
+    if (gameState.playState !== GAME_PLAY_STATES.PLAYING) return;
+
+    const { target } = event;
+    let targetCell = childInfo?.cell;
+
+    if (!(targetCell ?? false)) {
+        const [, x, y] = target.id.split("-");
+        targetCell = board[x][y];
+    }
+
+    switch (type) {
+        case EVENTS.ARROWLEFT:
+        case EVENTS.ARROWUP:
+        case EVENTS.ARROWRIGHT:
+        case EVENTS.ARROWDOWN:
+            const neighbours = calculateNeighbouringCells(board, targetCell);
+            const coord = COORDS[type];
+            const toFocusCell = neighbours.find(c => targetCell.x + coord[0] === c.x && targetCell.y + coord[1] === c.y);
+            const toFocus = document.querySelector(`#cell-${toFocusCell?.x}-${toFocusCell?.y}`);
+
+            toFocus?.focus();
+        break;
+        case EVENTS.PRIMARYCLICK:
+            if (!targetCell.hidden) return;
+            
+            let emptyCells = calculateEmptyCells(targetCell);
+            board = board.map(row => row.map(c => emptyCells.some(_c => _c.x === c.x && _c.y === c.y) ? { ...c, hidden: false } : c));
+
+            if (targetCell.type === CELL_TYPES.BOMB) {
+                gameState.playState = GAME_PLAY_STATES.LOSING;
+
+                let i = 0;
+                board.forEach((row, x) => row.forEach((cell, y) => {
+                    if (cell.type === CELL_TYPES.BOMB) {
+                        i++;
+                        setTimeout(() => {
+                            console.log("ran bomb timeout");
+                            board[x][y] = {
+                                ...cell,
+                                hidden: false,
+                                flagged: false
+                            };
+
+                            m.redraw()
+                        }, i * 100 + 500);
+                    }
+                }));
+
+                setTimeout(() => {
+                    console.log("ran menu timeout")
+                    gameState.playState = GAME_PLAY_STATES.ENDED;
+                    m.redraw();
+                }, i * 100 + 1500);
+            } else checkWin();
+        break;
+        case EVENTS.SECONDARYCLICK:
+        case EVENTS.F:
+            if (!targetCell.hidden) return;
+            let cell = board[targetCell.x][targetCell.y];
+            cell.flagged = !cell.flagged;
+
+            checkWin();
+        break;
+    }
+
+    function checkWin() {
+        if (board.every(row => row.every(cell => cell.type === CELL_TYPES.BOMB ? cell.flagged : !cell.flagged))) {
+            gameState.playState = GAME_PLAY_STATES.WINNING;
+
+            // TODO: Add some animations
+
+            setTimeout(() => {
+                gameState.playState = GAME_PLAY_STATES.ENDED;
+                gameState.won = true;
+                m.redraw();
+            }, 500);
+        }
+    }
+}
+
+function formatTime(ms) {
+    return new Date(ms).toISOString().substring(14, 19);
+}
+
+function resetStates() {
+    mode ??= MODES.easy;
+    board = generateBoard();
+    gameState = {
+        playState: GAME_PLAY_STATES.PLAYING,
+        timeElapsed: 0,
+        won: false
+    };
+}
+
 // #endregion
 
 // #region Components
 
 const Cell = {
     view({ attrs }) {
-        const { x, y, type, hidden, neighbourBombs } = attrs.cell;
+        const { x, y, type, hidden, neighbourBombs, flagged } = attrs.cell;
 
         return m(`button.cell`, {
+            id: `cell-${x}-${y}`,
             style: `
                 grid-column: ${x + 1};
                 grid-row: ${y + 1};
             `,
-            class: `${hidden ? "hidden" : type + (type === CELL_TYPES.NUM ? "-" + neighbourBombs : "")}`,
-            onclick() {
-                if (!hidden) return;
-                let emptyCells = calculateEmptyCells(attrs.cell);
-                board = board.map(row => row.map(c => emptyCells.some(_c => _c.x === c.x && _c.y === c.y) ? { ...c, hidden: false } : c));
-
-                if (type === CELL_TYPES.BOMB) {
-                    console.log("you lost :(");
-                }
+            tabindex: gameState.playState === GAME_PLAY_STATES.PLAYING ? "0" : "-1",
+            class: `${flagged ? "flagged" : hidden ? "hidden" : type + (type === CELL_TYPES.NUM ? "-" + neighbourBombs : "")} type-${type}`,
+            onclick(event) {
+                eventHandler(event.button === 0 ? EVENTS.PRIMARYCLICK : EVENTS.SECONDARYCLICK, event, { cell: attrs.cell });
             }
         });
     }
 };
 
+const HowToPlay = {
+    view() {
+        return m("details", [
+            m("summary", "How to play"),
+            m("p", "There are hidden bombs scattered around the map. Dig cells to clear areas unlikely to contain bombs. Some cells are numbered, indicating the number of bombs around them. Use big brain logic to flag cells likely to contain bombs. Once all bomb-containing cells are flagged and all other cells cleared, you win! ...or you may dig a cell containing a bomb, in which case you lose"),
+            m("h2", "Controls"),
+            m("ul", [
+                m("li", [m("code", "TAB"), "or", m("code", "Arrow Keys"), "- Select cells"]),
+                m("li", [m("code", "SPACE"), "or", m("code", "Left Click"), "- Dig cells"]),
+                m("li", [m("code", "F"), "or", m("code", "Right Click"), "- Flag a cell"])
+            ])
+        ]);
+    }
+}
+
 const Options = {
     view() {
-        return m()
+        return m("form", {
+            onsubmit(event) {
+                event.preventDefault();
+            }
+        }, [
+            m("label", { for: "game-mode-select" }, "Game mode: "),
+            m("select", {
+                id: "game-mode-select",
+                onchange(event) {
+                    event.preventDefault();
+                    mode = MODES[event.target.value];
+                    resetStates();
+                }
+            }, Object.values(MODES).map(({ name, bombs }) => m("option", { value: name, selected: name === mode.name }, `${[name[0].toUpperCase(), ...name.split("").slice(1)].join("")} (${bombs} bombs)`)))
+        ]);
+    }
+};
+
+const Info = {
+    view() {
+        return m("div.info", [
+            m("header", [
+                m("h1", "Minesweeper!"),
+                m("p", ["created with love by ", m("a", { href: "https://github.com/code913/minesweeper" }, "code913")]),
+            ]),
+            m(HowToPlay),
+            m(Options)
+        ]);
+    }
+};
+
+const Menu = {
+    view() {
+        const { timeElapsed, won } = gameState;
+
+        return m("div.menu", [
+            m("section.menu-content", [
+                m("h2", { class: won ? "winner" : "loser" }, `You ${won ? "won :)" : "lost :("}`),
+                m("ul", [
+                    m("li", ["Time taken: ", formatTime(timeElapsed) ])
+                ]),
+                m("button", {
+                    oncreate(vnode) {
+                        vnode.dom.focus();
+                    },
+                    onclick() {
+                        resetStates();
+                        document.querySelector("#cell-0-0").focus();
+                    }
+                }, "Restart game")
+            ])
+        ]);
     }
 };
 
 const Board = {
     view() {
-        return m("div", {
-            class: "board",
+        return m("div.board", {
             style: `
                 width: ${BOARD_SIZE[0] * 2}rem;
                 height: ${BOARD_SIZE[1] * 2}rem;
-                display: grid;
                 grid-template-columns: repeat(${BOARD_SIZE[0]}, 1fr);
                 grid-template-rows: repeat(${BOARD_SIZE[1]}, 1fr);
-            `
-        }, board.map(row => row.map(cell => m(Cell, { cell })).flat(1)));
+            `,
+            onkeydown(e) {
+                const { target, key } = e;
+                
+                if (target.classList.contains("cell")) {
+                    let type = EVENTS[key.toUpperCase()];
+
+                    if (type) eventHandler(type, e);
+                }
+            }
+        }, board.map(row => row.map(cell => m(Cell, { cell }))).flat(2));
     }
 };
 
 const Main = {
     view() {
-        return m("main", {
-            style: `
-                display: grid;
-                place-items: center;
-                height: 100vh;
-                width: 100vw;
-            `
-        }, [m(Board)])
+        return [
+            m(Info),
+            m("div.container", [
+                m(Board),
+                gameState.playState === GAME_PLAY_STATES.ENDED ? m(Menu) : null
+            ])
+        ];
     }
 };
 
